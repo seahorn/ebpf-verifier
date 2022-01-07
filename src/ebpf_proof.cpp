cd499af14c967a401c14c08aa0840d47ab818a0b
Jorge Navas <navasjorgea@gmail.com>
2022-01-07 19:27:09 +0100

    refactor: added stubs for a proof generator
     Conflicts:
    	src/crab_verifier.cpp
    
     Author:    Jorge Navas <navasjorgea@gmail.com>
     Date:      Fri Jan 7 19:27:09 2022 +0100
    
     On branch merging-type-proofs-with-main
     You are currently cherry-picking commit 067bf2f.
    
     Changes to be committed:
    	modified:   src/crab_verifier.cpp
    	modified:   src/crab_verifier.hpp
    	new file:   src/ebpf_proof.cpp
    	new file:   src/ebpf_proof.hpp
    	modified:   src/main/check.cpp
    	modified:   src/main/utils.hpp
    	modified:   src/test/test_verify.cpp

---
+++ src/ebpf_proof.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+#include "ebpf_proof.hpp"
+
+bool ebpf_generate_proof(std::ostream& s, const InstructionSeq& prog, const program_info& info,
+                         const ebpf_verifier_options_t* options, const crab_results& results) {
+    if (!results.pass_verify()) {
+        // If the program is not correct then we cannot generate a proof
+        return false;
+    }
+
+    /*
+      The goal is to translate results.pre_invariants and
+      results.post_invariants into types from our type system. For that,
+      we will need first to define all the C++ types/classes needed for
+      defining our type system (for pointer, checked_pointer, etc), and
+      then the conversion.
+
+      A type checker is not part of the proof but we should also
+      implement it so that we can be sure that our types are correct.
+     */
+
+    s << "TODO: proof\n";
+    return false;
+}
--- src/main/utils.hpp
+++ src/main/utils.hpp
@@ -5,10 +5,10 @@ template<typename F>
 auto timed_execution(F f) {
     clock_t begin = clock();
 
-    const auto& res = f();
+    auto res = f();
 
     clock_t end = clock();
 
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    return std::make_tuple(res, elapsed_secs);
+    return std::make_tuple(std::move(res), elapsed_secs);
 }
