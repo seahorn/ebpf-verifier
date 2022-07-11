28e05ecd332e6e1833237be7ed22341d95512bad
Ameer Hamza <ah18r@my.fsu.edu>
2022-07-11 19:26:29 -0400

    Added more structure to implementation; Implemented basic methods
    Signed-off-by: Ameer Hamza <ah18r@my.fsu.edu>

--- src/crab/offset_domain.cpp
+++ src/crab/offset_domain.cpp
@@ -1,64 +1,167 @@
 #include "crab/offset_domain.hpp"
 
-//ctx_offsets_t::ctx_offsets_t(const ebpf_context_descriptor_t* desc) {
-//    if (desc->data != -1)
-//        m_offsets[desc->data] = node_t(0);
-//    if (desc->end != -1)
-//        m_offsets[desc->end] = node_t(1);
-//    //if (desc->meta != -1)
-//        //m_offsets[desc->meta] = node_t();
-//}
+void registers_state_t::set_to_top() {
+    m_reg_dists = register_dists_t{nullptr};
+    m_is_bottom = false;
+}
+
+void registers_state_t::set_to_bottom() {
+    m_reg_dists = register_dists_t{nullptr};
+    m_is_bottom = true;
+}
+
+bool registers_state_t::is_top() const {
+    if (m_is_bottom) return false;
+    for (auto &it : m_reg_dists) {
+        if (it != nullptr) return false;
+    }
+    return true;
+}
+
+bool registers_state_t::is_bottom() const {
+    return m_is_bottom;
+}
+
+void stack_state_t::set_to_top() {
+    m_stack_slot_dists.clear();
+    m_is_bottom = false;
+}
+
+void stack_state_t::set_to_bottom() {
+    m_stack_slot_dists.clear();
+    m_is_bottom = true;
+}
+
+bool stack_state_t::is_top() const {
+    if (m_is_bottom) return false;
+    return m_stack_slot_dists.empty();
+}
+
+bool stack_state_t::is_bottom() const {
+    return m_is_bottom;
+}
+
+void extra_constraints_t::set_to_top() {
+    m_eq = forward_and_backward_eq_t();
+    m_ineq = inequality_t();
+}
+
+void extra_constraints_t::set_to_bottom() {
+    m_is_bottom = true;
+}
+
+bool extra_constraints_t::is_top() const {
+    if (m_is_bottom) return false;
+    return (m_eq.m_forw.m_slack == boost::none && m_ineq.m_slack == boost::none);
+}
+
+bool extra_constraints_t::is_bottom() const {
+    return m_is_bottom;
+}
+
+ctx_t::ctx_t(const ebpf_context_descriptor_t* desc) {
+    if (desc->data != -1)
+        m_dists[desc->data] = dist_t(0);
+    if (desc->end != -1)
+        m_dists[desc->end] = dist_t(-1);
+    //if (desc->meta != -1)
+        //m_offsets[desc->meta] = node_t();
+}
 
 offset_domain_t offset_domain_t::setup_entry() {
-    //std::shared_ptr<ctx_offsets_t> ctx = std::make_shared<ctx_offsets_t>(global_program_info.type.context_descriptor);
-    //m_offset_graph_t graph(global_program_info.type.context_descriptor);
+    std::shared_ptr<ctx_t> ctx = std::make_shared<ctx_t>(global_program_info.type.context_descriptor);
 
-    //offset_domain_t off_d(ctx, graph);
-    offset_domain_t off_d;
+    offset_domain_t off_d(ctx);
     return off_d;
 }
 
-offset_domain_t offset_domain_t::bottom() { offset_domain_t t; return t; }
-void offset_domain_t::set_to_top() {}
-void offset_domain_t::set_to_bottom() {}
-bool offset_domain_t::is_bottom() const { return m_is_bottom; }
-bool offset_domain_t::is_top() const { return false; }
+offset_domain_t offset_domain_t::bottom() {
+    offset_domain_t off;
+    off.set_to_bottom();
+    return off;
+}
+
+void offset_domain_t::set_to_top() {
+    m_reg_state.set_to_top();
+    m_stack_state.set_to_top();
+    m_extra_constraints->set_to_top();
+}
+
+void offset_domain_t::set_to_bottom() {
+    m_is_bottom = true;
+}
+
+bool offset_domain_t::is_bottom() const {
+    return m_is_bottom;
+}
+
+bool offset_domain_t::is_top() const {
+    if (m_is_bottom) return false;
+    return (m_reg_state.is_top() && m_stack_state.is_top() && m_extra_constraints->is_top());
+}
+
 // inclusion
 bool offset_domain_t::operator<=(const offset_domain_t& other) const { return true; }
+
 // join
 void offset_domain_t::operator|=(const offset_domain_t& abs) {}
+
 void offset_domain_t::operator|=(offset_domain_t&& abs) {}
+
 offset_domain_t offset_domain_t::operator|(const offset_domain_t& other) const { return other; }
+
 offset_domain_t offset_domain_t::operator|(offset_domain_t&& abs) const { return abs; }
+
 // meet
 offset_domain_t offset_domain_t::operator&(const offset_domain_t& other) const { return other; }
+
 // widening
 offset_domain_t offset_domain_t::widen(const offset_domain_t& other) const { return other; }
+
 // narrowing
 offset_domain_t offset_domain_t::narrow(const offset_domain_t& other) const { return other; }
+
 //forget
 void offset_domain_t::operator-=(variable_t var) {}
 
 void offset_domain_t::write(std::ostream& os) const {}
-std::string offset_domain_t::domain_name() const { return "offset_domain"; }
+
+std::string offset_domain_t::domain_name() const {
+    return "offset_domain";
+}
+
 int offset_domain_t::get_instruction_count_upper_bound() { return 0; }
+
 string_invariant offset_domain_t::to_set() { return string_invariant{}; }
+
 void offset_domain_t::set_require_check(check_require_func_t f) {}
 
 void offset_domain_t::operator()(const Assume &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const Bin &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const Undefined &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const Un &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const LoadMapFd &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const Call &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const Exit &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const Jmp &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const Packet &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const LockAdd &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const Assert &, location_t loc, int print) {}
+
 void offset_domain_t::operator()(const basic_block_t& bb, bool check_termination, int print) {}
 
 void offset_domain_t::do_mem_store(const Mem& b, const Reg& target_reg, location_t, int print) {}
+
 void offset_domain_t::do_load(const Mem& b, const Reg& target_reg, location_t loc, int print) {
     int offset = b.access.offset;
     Reg basereg = b.access.basereg;
--- src/crab/offset_domain.hpp
+++ src/crab/offset_domain.hpp
@@ -7,11 +7,10 @@
 #include "linear_constraint.hpp"
 #include "string_constraints.hpp"
 
-using register1_t = unsigned int;
 using constant_t = int;   // define a domain for constants
 //using symbol_t = register_t;    // a register with unknown value
 using weight_t = constant_t; // should be constants + symbols
-using slack_var_t = std::string;
+using slack_var_t = boost::optional<std::string>;
 
 enum class rop_t {
     R_GT,
@@ -23,31 +22,87 @@ enum class rop_t {
 struct dist_t {
     slack_var_t m_slack;
     weight_t m_dist;
+
+    dist_t(weight_t d, slack_var_t s = boost::none) : m_slack(s), m_dist(d) {}
+    dist_t() : m_slack(boost::none), m_dist(0) {}
 };      // if dist is +ve, represents `begin+dist+slack;`, if dist is -ve, represents `end+dist+1`
 
 struct inequality_t {
     slack_var_t m_slack;
     rop_t m_rel;
     weight_t m_value;
+
+    inequality_t(slack_var_t slack, rop_t rel, weight_t val) : m_slack(slack), m_rel(rel)
+                                                               , m_value(val) {}
+    inequality_t() = default;
 };    // represents `slack rel value;`, e.g., `s >= 0`
 
-using forward_and_backward_eq_t = std::pair<dist_t, dist_t>;    // represents constraint `p[0] = p[1];`, e.g., `begin+8+s = end`
+struct forward_and_backward_eq_t {
+    dist_t m_forw;
+    dist_t m_backw;
 
-using extra_constraints_t = std::pair<forward_and_backward_eq_t, inequality_t>;
+    forward_and_backward_eq_t(dist_t forw, dist_t backw) : m_forw(forw), m_backw(backw) {}
+    forward_and_backward_eq_t() = default;
+};  // represents constraint `p[0] = p[1];`, e.g., `begin+8+s = end`
 
-using register_dists_t = std::array<dist_t, 11>;        // represents `rn = dist;`, where n \belongs [0,10], e.g., `r1 = begin+8`
 
-using stack_dists_t = std::unordered_map<unsigned int, dist_t>;    // represents `sp[n] = dist;`, where n \belongs [0,511], e.g., `sp[508] = begin+16`
+using register_dists_t = std::array<std::shared_ptr<dist_t>, 11>;        // represents `rn = dist;`, where n \belongs [0,10], e.g., `r1 = begin+8`
 
-using ctx_t = std::unordered_map<unsigned int, dist_t>;    // represents `cp[n] = dist;`
+class registers_state_t {
 
+    register_dists_t m_reg_dists;
+    bool m_is_bottom = false;
+
+    public:
+        registers_state_t(bool is_bottom = false) : m_is_bottom(is_bottom) {}
+        void set_to_top();
+        void set_to_bottom();
+        bool is_bottom() const;
+        bool is_top() const;
+};
+
+class stack_state_t {
+    using stack_slot_dists_t = std::unordered_map<unsigned int, dist_t>;    // represents `sp[n] = dist;`, where n \belongs [0,511], e.g., `sp[508] = begin+16`
+
+    stack_slot_dists_t m_stack_slot_dists;
+    bool m_is_bottom = false;
+
+    public:
+        stack_state_t(bool is_bottom = false) : m_is_bottom(is_bottom) {}
+        void set_to_top();
+        void set_to_bottom();
+        bool is_bottom() const;
+        bool is_top() const;
+};
+
+class extra_constraints_t {
+    forward_and_backward_eq_t m_eq;
+    inequality_t m_ineq;
+    bool m_is_bottom = false;
+
+    public:
+        extra_constraints_t(bool is_bottom = false) : m_is_bottom(is_bottom) {}
+        void set_to_top();
+        void set_to_bottom();
+        bool is_bottom() const;
+        bool is_top() const;
+};
+
+class ctx_t {
+    using ctx_dists_t = std::unordered_map<unsigned int, dist_t>;    // represents `cp[n] = dist;`
+
+    ctx_dists_t m_dists;
+
+    public:
+        ctx_t(const ebpf_context_descriptor_t* desc);
+};
 
 class offset_domain_t final {
 
     bool m_is_bottom = false;
-    register_dists_t m_reg_dists;
-    stack_dists_t m_stack_dists;
-    extra_constraints_t m_extra_constraints;
+    registers_state_t m_reg_state;
+    stack_state_t m_stack_state;
+    std::shared_ptr<extra_constraints_t> m_extra_constraints;
     std::shared_ptr<ctx_t> m_ctx_dists;
 
   public:
@@ -57,7 +112,7 @@ class offset_domain_t final {
     offset_domain_t(const offset_domain_t& o) = default;
     offset_domain_t& operator=(offset_domain_t&& o) = default;
     offset_domain_t& operator=(const offset_domain_t& o) = default;
-    //offset_domain_t(crab::register_types_t&& _types, crab::stack_t&& _st, std::shared_ptr<crab::ctx_t> _ctx) = default;
+    offset_domain_t(std::shared_ptr<ctx_t> _ctx) : m_ctx_dists(_ctx) {}
     static offset_domain_t setup_entry();
     // bottom/top
     static offset_domain_t bottom();
